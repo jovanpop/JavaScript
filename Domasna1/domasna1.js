fd8db2effc2ae047bc791e28d4d9371278ef9131
jovanpop <jovan.popovski@outlook.com>
2021-05-08 00:02:04 +0200

    domasna1

---
+++ Domasna1/domasna1.js
@@ -0,0 +1,31 @@
+let name='Tom';
+let age=25;
+let phone='075678901';
+console.log(name);
+console.log(phone);
+console.log(age);
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof phone);
+const embg=2508995430085;
+console.log (embg);
+/* const embg=1234567891001;
+console.log (embg); */ /* ERROR */
+
+let studentOne={
+    fName:'Rebeca',
+    surName:'Phillips',
+    age:30
+};
+let studentTwo={
+    fName:'John',
+    surName:'Peterson',
+    age:32
+};
+console.log(studentOne);
+console.log(studentTwo);
+console.log(typeof studentOne);
+console.log(typeof studentTwo);
+let generation=[studentOne,studentTwo];
+console.log(generation);
+console.log(typeof generation);
